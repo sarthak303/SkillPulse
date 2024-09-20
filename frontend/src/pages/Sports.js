62e863b0e9f42c6e440ed9232ca2472da5fb4a8a
Prakhar Agrawal <prakhar.agra10@gmail.com>
2024-09-21 01:35:19 +0530

    small chang

--- frontend/src/pages/Sports.js
+++ frontend/src/pages/Sports.js
@@ -3,10 +3,10 @@ import './Sports.css';
 
 const Sports = () => {
     const sportsData = [
-        { id: 1, title: 'Badminton', image: require('../assets/sport1.png') },
+        { id: 1, title: 'Cricket', image: require('../assets/sport4.png') },
         { id: 2, title: 'Football', image: require('../assets/sport2.png') },
         { id: 3, title: 'Tennis', image: require('../assets/sport3.png') },
-        { id: 4, title: 'Cricket', image: require('../assets/sport4.png') },
+        { id: 4, title: 'Badminton', image: require('../assets/sport1.png') },
     ];
       
 
